dcbb192adf17d44f8511ccff28c31c204fa8e2a2
Yuri <103314031+Khalil-0B09@users.noreply.github.com>
2022-05-16 10:03:31 +0200

    Commit update

---
+++ UML-main/UML-main/Code/Mine.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Mine {
+
+    int idMine;
+    int typeMinerais;
+    int nbMinerais;
+
+    public Mine(int idMine, int typeMinerais, int nbMinerais) {
+        this.idMine = idMine;
+        this.typeMinerais = typeMinerais;
+        this.nbMinerais = nbMinerais;
+    }
+}
+    /**
+     *
+     */
+    public idMine int;
+
+    /**
+     *
+     */
+    public typeMinerais int;
+
+    /**
+     *
+     */
+    public nbMinerais int;
+
+    /**
+     *
+     */
+    private Entrepots entrepots;
+
+    /**
+     *
+     */
+    private Robot robot;
+
+    /**
+     *
+     */
+    private Set<Terrain> terrain;
+
+}
